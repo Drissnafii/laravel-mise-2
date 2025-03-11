3b8f07f0df1826adbb84f9c31e6a307fb9398211
drissnafii <drissnafi3@gmail.com>
2025-03-11 10:36:31 +0000

    Update Article and Tag factories to include title, description, and name fields

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -17,7 +17,8 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->name(),
+            'description' => fake()->text(20)
         ];
     }
 }
